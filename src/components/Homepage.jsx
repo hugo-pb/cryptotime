372bf75dd8fa576b9e9abfcb19f7306d003d092a
Hugo <h.palomerabecerra@gmail.com>
2022-03-29 15:20:38 -0600

    fidex responsive navbar

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import millify from "millify";
-import { Typography, Row, Col, Statistic } from "antd";
+import { Typography,  Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/CryptoApi";
 
@@ -12,54 +12,49 @@ const Homepage = () => {
 
   if (isFetching) return "loading..";
   return (
-    <>
+    <div className="container">
       <Title level={2} className="heading" style={{ textAlign: "center" }}>
         Global Crypto Stats
       </Title>
 
-      <Row>
-        <Col span={12}>
+      <div className="row">
+        <div className="col-md-6">
           <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
-        </Col>
-        <Col span={12}>
+        </div>
+        <div className="col-md-6">
           <Statistic
             title="Total Exchange"
             value={millify(globalStats.totalExchanges)}
           />
-        </Col>
-        <Col span={12}>
+        </div>
+        <div className="col-md-6">
           <Statistic
             title="Total Market Cap"
             value={millify(globalStats.totalMarketCap)}
           />
-        </Col>
-        <Col span={12}>
+        </div>
+        <div className="col-md-6">
           <Statistic
             title="Total 24H Volume"
             value={millify(globalStats.total24hVolume)}
           />
-        </Col>
-        <Col span={12}>
+        </div>
+        <div className="col-md-6">
           <Statistic
             title="Total Markets"
             value={millify(globalStats.totalMarkets)}
           />
-        </Col>
-      </Row>
+        </div>
+      </div>
 
       <div className="home-heading-container">
-        <Row>
-          <Title style={{ textAlign: "center" }} level={2}>
-            Top 10 Cryptocurrencies in the world
-          </Title>
-        </Row>
-        <Row>
-          <Title className="Show-more">
-            <Link to="/cryptocurrencies">Show more</Link>
-          </Title>
-        </Row>
+        <Title style={{ textAlign: "center" }} level={2}>
+          Top 10 Cryptocurrencies in the world
+        </Title>
+
+        <Link to="/cryptocurrencies">Show more</Link>
       </div>
-    </>
+    </div>
   );
 };
 export default Homepage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,104 @@
 import React from "react";
-import { Button, Menu, Avatar, Typography } from "antd";
+import { Avatar, Typography } from "antd";
 import { Link } from "react-router-dom";
-import {
-  HomeOutlined,
-  MoneyCollectOutlined,
-  BulbOutlined,
-  FundOutlined,
-
-} from "@ant-design/icons";
+import { MoneyCollectOutlined, BulbOutlined } from "@ant-design/icons";
 import Icon from "../images/cryptocurrency.png";
-import '../styles/Navbar.css'
+import "../styles/Navbar.css";
 const Navbar = () => {
   return (
-    <div className="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <Avatar src={Icon} size="large" />
-        <Typography.Title level={2} className="logo">
-          <Link to="/">CryptoTime</Link>
-        </Typography.Title>
-        <Button className="menu-control-container"></Button>
-      </div>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link to="./">Home</Link>
-        </li>
-        <li className="nav-item" icon={<FundOutlined />}>
-          <Link to="/cryptocurrencies">Cryptocurrencies</Link>
-        </li>
-        <li className="nav-item" icon={<MoneyCollectOutlined />}>
-          <Link to="/exchanges">Exchanges</Link>
-        </li>
-        <li className="nav-item" icon={<BulbOutlined />}>
-          <Link to="/news">News</Link>
-        </li>
-      </ul>
+    <div className="Navbar">
+      {/* <nav classNameName="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <Avatar src={Icon} size="large" />
+            <Typography.Title level={2} classNameName="logo">
+              <Link to="/">CryptoTime</Link>
+            </Typography.Title>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul classNameName="navbar-nav me-auto mb-2 mb-lg-0">
+                <li classNameName="nav-item">
+                  <a classNameName="nav-link" href="./">
+                    Home
+                  </a>
+                </li>
+                <li classNameName="nav-item">
+                  <a classNameName="nav-link" href="/cryptocurrencies">
+                    Cryptocurrencies
+                  </a>
+                </li>
+                <li classNameName="nav-item" icon={<MoneyCollectOutlined />}>
+                  <a classNameName="nav-link" href="/exchanges">
+                    Exchanges
+                  </a>
+                </li>
+                <li classNameName="nav-item" icon={<BulbOutlined />}>
+                  <a classNameName="nav-link" href="/news">
+                    News
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+    </nav> */}
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a class="navbar-brand" href="./">
+            <img
+              src={Icon}
+              alt="icon"
+              width="30"
+              heigh
+                          className="d-inline-block align-text-top"
+                          style={{marginRight:'1rem'}}
+            />
+         CryptoTime
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a classNameName="nav-link" href="./">
+                Home
+              </a>
+
+              <a classNameName="nav-link" href="/cryptocurrencies">
+                Cryptocurrencies
+              </a>
+
+              <a classNameName="nav-link" href="/exchanges">
+                Exchanges
+              </a>
+
+              <a classNameName="nav-link" href="/news">
+                News
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
